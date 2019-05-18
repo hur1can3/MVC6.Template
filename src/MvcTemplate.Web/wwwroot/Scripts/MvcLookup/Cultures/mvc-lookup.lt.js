cd425bd958e67e61e8f6b1406b13fe7a8929c50e
Muchiachio <aldis.rameika@gmail.com>
2019-05-18 14:30:09 +0300

    Additional example language removal
    - Removed Lithuanian as an example language, because project renaming doesn't remove it and doing it by hand takes some time.

--- src/MvcTemplate.Web/wwwroot/Scripts/MvcLookup/Cultures/mvc-lookup.lt.js
+++
@@ -1,9 +0,0 @@
-﻿if (document.documentElement.lang == 'lt')
-{
-    MvcLookup.prototype.lang = {
-        error: 'Įvyko klaida, susisiekite su administratoriumi',
-        select: 'Pasirinkti ({0})',
-        noData: 'Irašų nerasta',
-        search: 'Ieškoti...'
-    };
-}
